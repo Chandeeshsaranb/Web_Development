e6e7bedc80a0e060944a179e4f7dcc6ed8c90864
= <=>
2025-07-04 11:31:12 +0530

    feat : added a new task piano

--- frontend/javascript/piano/script.js
+++ frontend/javascript/piano/script.js
@@ -10,15 +10,26 @@ document.addEventListener('keydown', function(event) {
         'u': 'a7',
         'i': 'a8',
         'o': 'a9',
-        'p': 'a10'
+        'p': 'a10',
+        'a': 'a11',
+        's': 'a12',
+        'd': 'a13',
+        'f': 'a14',
+        'g': 'a15',
+        'h': 'a16',
+        'j': 'a17',
+        'k': 'a18',
+        'l': 'a19',
+        ';': 'a20'
     };
     if (keyMap[kp]) {
         const audio = document.getElementById(keyMap[kp]);
+        audio.parentElement.classList.add("active-press");
         audio.currentTime = 0;
         audio.play();
-        audio.parentElement.style.backgroundColor="grey"
     }
-});
+       
+})
 
 document.addEventListener('keyup', function(event) {
     let ke = event.key.toLowerCase();
@@ -32,11 +43,21 @@ document.addEventListener('keyup', function(event) {
         'u': 'a7',
         'i': 'a8',
         'o': 'a9',
-        'p': 'a10'
+        'p': 'a10',
+        'a': 'a11',
+        's': 'a12',
+        'd': 'a13',
+        'f': 'a14',
+        'g': 'a15',
+        'h': 'a16',
+        'j': 'a17',
+        'k': 'a18',
+        'l': 'a19',
+        ';': 'a20'
     };
     if (keyMap[ke]) {
         const audio = document.getElementById(keyMap[ke]);
-        audio.parentElement.style.backgroundColor="white"
+        audio.parentElement.classList.remove("active-press");
     }
 });
 
